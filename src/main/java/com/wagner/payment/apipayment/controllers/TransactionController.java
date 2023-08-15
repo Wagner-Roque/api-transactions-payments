e9fc8832c93e34c2182ad8c683fc13b15a1ee9d3
Wagner Roque <roquewagnertadeu@gmail.com>
2023-08-15 13:43:38 -0300

    feature: create controllers para Transaction e user

---
+++ src/main/java/com/wagner/payment/apipayment/controllers/TransactionController.java
@@ -0,0 +1,26 @@
+package com.wagner.payment.apipayment.controllers;
+
+import com.wagner.payment.apipayment.domain.transactions.Transaction;
+import com.wagner.payment.apipayment.dtos.TransactionDTO;
+import com.wagner.payment.apipayment.service.TransactionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(("/transactions"))
+public class TransactionController {
+
+    @Autowired
+    private TransactionService transactionService;
+
+    @PostMapping
+    public ResponseEntity<Transaction> createTransaction(@RequestBody TransactionDTO transactionDTO) throws Exception {
+        Transaction transaction = this.transactionService.createTransaction(transactionDTO);
+        return new ResponseEntity<>(transaction, HttpStatus.OK);
+    }
+}
---
+++ src/main/java/com/wagner/payment/apipayment/controllers/UserController.java
@@ -0,0 +1,30 @@
+package com.wagner.payment.apipayment.controllers;
+
+import com.wagner.payment.apipayment.dtos.UserDTO;
+import com.wagner.payment.apipayment.domain.user.UserEntity;
+import com.wagner.payment.apipayment.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(("/users"))
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+    @PostMapping
+    public ResponseEntity<UserEntity> createUser(@RequestBody UserDTO user){
+        UserEntity newUser = userService.createUser(user);
+        return new ResponseEntity<>(newUser, HttpStatus.CREATED);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<UserEntity>> getAllusers(){
+       List<UserEntity> users = this.userService.getAllUsers();
+       return new ResponseEntity<>(users, HttpStatus.OK);
+    }
+}
