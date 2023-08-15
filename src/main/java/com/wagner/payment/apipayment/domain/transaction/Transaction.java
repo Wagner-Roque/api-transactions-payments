fe5147edf0ba6761c45d9b0494033635713e3c72
Wagner Roque <roquewagnertadeu@gmail.com>
2023-08-15 10:45:55 -0300

    feature: create Transaction entity

---
+++ src/main/java/com/wagner/payment/apipayment/domain/transaction/Transaction.java
@@ -0,0 +1,33 @@
+package com.wagner.payment.apipayment.domain.transaction;
+
+import com.wagner.payment.apipayment.user.UserEntity;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity(name = "transactions")
+@Table(name = "transactions")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Transaction {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private BigDecimal amount;
+
+    @ManyToOne
+    @JoinColumn(name = "sender_id")
+    private UserEntity sender;
+
+    @ManyToOne
+    @JoinColumn(name = "receiver_id")
+    private UserEntity receiver;
+
+    private LocalDateTime timestamp;
+}
